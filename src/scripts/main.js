faa7268be75dae72960810122f23fbea802fd7d4
Liudmyla Panchenko <liudmyla.panchenko29@gmail.com>
2025-06-03 13:44:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = document.querySelectorAll('tr');
+
+const dataToAppend = [];
+
+for (let i = 0; i < rows.length; i++) {
+  dataToAppend.push(rows[i].cells[1].textContent.trim());
+}
+
+const insertIndex = table.rows[0].cells.length - 1;
+
+for (let i = 0; i < rows.length; i++) {
+  const newCell = table.rows[i].insertCell(insertIndex);
+
+  if (i === 0 || i === rows.length - 1) {
+    newCell.outerHTML = `<th>${dataToAppend[i]}</th>`;
+  } else {
+    newCell.textContent = dataToAppend[i];
+  }
+}
