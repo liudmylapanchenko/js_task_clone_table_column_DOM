51e733a500e527e4d83bbb53c4935986b89d677c
Liudmyla Panchenko <liudmyla.panchenko29@gmail.com>
2025-06-04 15:03:08 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,27 @@
 'use strict';
 
 const table = document.querySelector('table');
-const rows = table.rows;
 
-const dataToAppend = [];
+if (table) {
+  const rows = table.rows;
+  const dataToAppend = [];
 
-for (let i = 0; i < rows.length; i++) {
-  const cell = rows[i].cells[1];
+  for (let i = 0; i < rows.length; i++) {
+    const cell = rows[i].cells[1];
 
-  dataToAppend.push(cell ? cell.textContent.trim() : '');
-}
-
-let maxColumns = 0;
-
-for (let i = 0; i < rows.length; i++) {
-  maxColumns = Math.max(maxColumns, rows[i].cells.length);
-}
-
-const insertIndex = maxColumns - 1;
-
-for (let i = 0; i < rows.length; i++) {
-  if (rows[i].cells.length < maxColumns) {
-    rows[i].insertCell(-1);
+    if (cell) {
+      dataToAppend.push(cell.textContent.trim());
+    }
   }
 
-  const newCell = rows[i].insertCell(insertIndex);
+  for (let i = 0; i < rows.length; i++) {
+    const insertIndex = rows[i].cells.length - 1;
+    const newCell = rows[i].insertCell(insertIndex);
 
-  if (i === 0 || i === rows.length - 1) {
-    newCell.outerHTML = `<th>${dataToAppend[i]}</th>`;
-  } else {
-    newCell.textContent = dataToAppend[i];
+    if (i === 0 || i === rows.length - 1) {
+      newCell.outerHTML = `<th>${dataToAppend[i]}</th>`;
+    } else {
+      newCell.textContent = dataToAppend[i];
+    }
   }
 }
